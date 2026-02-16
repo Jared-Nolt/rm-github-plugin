1f49b5ab4d924c1dc4cf3c29c8882a8f83b989f9
Jared <jared@rosewood.us.com>
2026-02-16 08:55:22 -0500

    built plugin

---
+++ rm-github-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:  RM GitHub Plugin
+ * Version:      1.0.0
+ * Description:  A professional plugin with GitHub update integration.
+ * Author:       Your Name
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load the updater logic
+require_once plugin_dir_path( __FILE__ ) . 'updater/updater.php';
+
+// Initialize the updater class
+new RM_Plugin_Updater( __FILE__ );
